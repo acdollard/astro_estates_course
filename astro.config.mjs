ece8d33d040332f05827d123749572cce7e4974d
Alexander Dollard <alexdollard141@gmail.com>
2024-05-01 12:27:49 -0500

    struggling w/ graphql dotenv

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import { loadEnv } from "vite";
+const { SECRET_PASSWORD } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
